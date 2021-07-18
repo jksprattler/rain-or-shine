b5370fed35f4a26342583ba1d83c1a45a684db97
jksprattler <jenna.sprattler@gmail.com>
2021-07-18 18:16:47 -0500

    Add import schedule and requirements

--- rain-or-shine.py
+++ rain-or-shine.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 import requests
+import schedule
+import time
 
 params = {
   'access_key': '20632cae552122179bc1cf711773820a',
@@ -14,12 +16,18 @@ rain = api_response['current']['precip']
 shine = api_response['current']['uv_index']
 description = api_response['current']['weather_descriptions'][0]
 
-if rain > 5:
-  print(u'Today\'s precipitation forecast: ', rain)
-  print(u'Greater than 5% chance of rain today, better grab your umbrella!')
-elif shine > 3:
-  print(u'Today\'s UV Index forecast: ', shine)
-  print(u'UV Index greater than 3 today, better grab your sunscreen!')
-else:
-  print(u'Today\'s weather description forecast: ', description)
-  print(u'Should be a pleasant day, no need for umbrella or sunscreen!')
+def job():
+    if rain > 5:
+      print(u'Today\'s precipitation forecast: ', rain)
+      print(u'Greater than 5% chance of rain today, better grab your umbrella!')
+    elif shine > 3:
+      print(u'Today\'s UV Index forecast: ', shine)
+      print(u'UV Index greater than 3 today, better grab your sunscreen!')
+    else:
+      print(u'Today\'s weather description forecast: ', description)
+      print(u'Should be a pleasant day, no need for umbrella or sunscreen!')
+schedule.every(3).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
